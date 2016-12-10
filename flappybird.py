07fbceaa86cfa6f549bdae27028b67fcabbe6583
JakobSt <jakob@baecker-sturm.de>
2016-12-10 15:56:28 +0000

    New class controller

--- flappybird.py
+++ flappybird.py
@@ -6,17 +6,29 @@ import sys
 import random
 import serial
 
-ser = serial.Serial(
-
-        port='/dev/ttyAMA0',
-        baudrate = 9600,
-        parity=serial.PARITY_NONE,
-        stopbits=serial.STOPBITS_ONE,
-        bytesize=serial.EIGHTBITS,
-        timeout=0
-)
-
 numberOfPlayers = 1
+
+class Controller:
+    def __init__(self):
+        self.ser = serial.Serial(
+            port='/dev/ttyAMA0',
+            baudrate = 9600,
+            parity=serial.PARITY_NONE,
+            stopbits=serial.STOPBITS_ONE,
+            bytesize=serial.EIGHTBITS,
+            timeout=0
+        )
+    def update(self):
+        numberOfBytesInInputBuffer = self.ser.inWaiting()
+        if numberOfBytesInInputBuffer >= 14:
+            receivedBytes = self.ser.read(14)
+            clickerID = receivedBytes[8:12]
+            clickerState = receivedBytes[4]
+
+            if clickerState.encode('hex') == '10' and not FlappyBird.bird[birdID].dead:
+                FlappyBird.bird[birdID].jump = 17
+                FlappyBird.bird[birdID].gravity = 5
+                FlappyBird.bird[birdID].jumpSpeed = 10
 
 class Bird:
     def __init__(self,id):
@@ -27,6 +39,7 @@ class Bird:
         self.birdY = 350
         self.wallx = 350 + id* 50
         self.id = id
+        self.clickerID = '00000000'
         self.dead = False
         self.sprite = 0
         self.jump = 0
@@ -36,7 +49,7 @@ class Bird:
         
 class FlappyBird:
     def __init__(self):
-        self.screen = pygame.display.set_mode((640,480))
+        self.screen = pygame.display.set_mode((400,708))
         self.background = pygame.image.load("assets/background.png").convert()
         self.wallUp = pygame.image.load("assets/bottom.png").convert_alpha()
         self.wallDown = pygame.image.load("assets/top.png").convert_alpha()
@@ -45,6 +58,8 @@ class FlappyBird:
         self.bird = [numberOfPlayers]
         for i in xrange(0,numberOfPlayers):
             self.bird[i] = Bird(i)
+        self.bird[0].clickerID = '01a7fd15'
+        self.controller = Controller()
         
     def updateWalls(self):
         self.bird[0].wallx -= 2
@@ -90,17 +105,8 @@ class FlappyBird:
         birdID = 0
         while True:
             clock.tick(60)
-            
-            bytesWaiting = ser.inWaiting()
-            if bytesWaiting == 14:
-                x=ser.read(14)
-                en_id = x[8:12]
-                state = x[4]
-                
-                if state.encode('hex') == '10' and not self.bird[birdID].dead:
-                    self.bird[birdID].jump = 17
-                    self.bird[birdID].gravity = 5
-                    self.bird[birdID].jumpSpeed = 10
+
+            self.controller.update()
 
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
