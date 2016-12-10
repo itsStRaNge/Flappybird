3b0245b1c4c2a42f451201bc1f39434a7c5bcd64
Lukas Bernhard <L.J.Bernhard@web.de>
2016-12-10 16:43:35 +0100

    first working version of multiple birds

--- flappybird.py
+++ flappybird.py
@@ -4,23 +4,28 @@ import pygame
 from pygame.locals import *  # noqa
 import sys
 import random
-numberOfPlayers = 1
-
+numberOfPlayers = 2
+wallSpawn = 1000
+distanceOfBirds = 126
+keys = [K_UP,K_DOWN,K_RIGHT,K_LEFT]
+names = ["nico","clemens","luis","jakob"]
 class Bird:
     def __init__(self,id):
-        self.bird = pygame.Rect(65, 50, 50, 50)
+	self.id = id
+	self.name = names[id]
+        self.positionX = distanceOfBirds * id + 70
+        self.bird = pygame.Rect(self.positionX, 50, 50, 50)
         self.birdSprites = [pygame.image.load("assets/1.png").convert_alpha(),
                             pygame.image.load("assets/2.png").convert_alpha(),
                             pygame.image.load("assets/dead.png")]
         self.birdY = 350
-	self.wallx = 350 + id* 50
-	self.id = id
         self.dead = False
         self.sprite = 0
         self.jump = 0
         self.jumpSpeed = 10
         self.gravity = 10
         self.counter = 0
+        self.key = keys[id]
         
 class FlappyBird:
     def __init__(self):
@@ -29,13 +34,14 @@ class FlappyBird:
         self.wallUp = pygame.image.load("assets/bottom.png").convert_alpha()
         self.wallDown = pygame.image.load("assets/top.png").convert_alpha()
         self.gap = 130
+        self.wallx = wallSpawn
         self.offset = random.randint(-110, 110)
         self.bird = [Bird(i) for i in range(0,numberOfPlayers)]
 
     def updateWalls(self):
-        self.bird[0].wallx -= 2
-        if self.bird[0].wallx < -80:
-            self.bird[0].wallx = 350 + 50 * self.bird[0].id
+        self.wallx -= 2
+        if self.wallx < -80:
+            self.wallx = wallSpawn
             self.bird[0].counter += 1
             self.offset = random.randint(-110, 110)
 
@@ -48,11 +54,11 @@ class FlappyBird:
             self.bird[birdID].birdY += self.bird[birdID].gravity
             self.bird[birdID].gravity += 0.2
         self.bird[birdID].bird[1] = self.bird[birdID].birdY
-        upRect = pygame.Rect(self.bird[birdID].wallx,
+        upRect = pygame.Rect(self.wallx,
                              360 + self.gap - self.offset + 10,
                              self.wallUp.get_width() - 10,
                              self.wallUp.get_height())
-        downRect = pygame.Rect(self.bird[birdID].wallx,
+        downRect = pygame.Rect(self.wallx,
                                0 - self.gap - self.offset - 10,
                                self.wallDown.get_width() - 10,
                                self.wallDown.get_height())
@@ -65,7 +71,7 @@ class FlappyBird:
             self.bird[birdID].birdY = 50
             self.bird[birdID].dead = False
             self.bird[birdID].counter = 0
-            self.bird[birdID].wallx = 350 + 50 * self.bird[birdID].id
+            self.wallx = wallSpawn
             self.offset = random.randint(-110, 110)
             self.bird[birdID].gravity = 5
 
@@ -73,14 +79,23 @@ class FlappyBird:
         clock = pygame.time.Clock()
         pygame.font.init()
         font = pygame.font.SysFont("Arial", 50)
-        birdID = 0
+       
         while True:
             clock.tick(60)
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     sys.exit()
-                if (event.type == pygame.KEYDOWN ) and not self.bird[birdID].dead: #or event.type == pygame.MOUSEBUTTONDOWN
-                    if(event.key == K_UP) :
+                if (event.type == pygame.KEYDOWN ) : #and not self.bird[birdID].dead
+                    birdID = -1
+                    if(event.key == keys[0]) :
+                        birdID = 0
+                    elif (event.key == keys[1]) :
+                        birdID = 1
+                    elif (event.key == keys[2]) :
+                        birdID = 2
+                    elif (event.key == keys[3]) :
+                        birdID = 3
+                    if(birdID != -1 and birdID < numberOfPlayers) :
                         self.bird[birdID].jump = 17
                         self.bird[birdID].gravity = 5
                         self.bird[birdID].jumpSpeed = 10
@@ -88,24 +103,24 @@ class FlappyBird:
             self.screen.fill((255, 255, 255))
             self.screen.blit(self.background, (0, 0))
             self.screen.blit(self.wallUp,
-                             (self.bird[birdID].wallx, 360 + self.gap - self.offset))
+                (self.wallx, 360 + self.gap - self.offset))
             self.screen.blit(self.wallDown,
-                             (self.bird[birdID].wallx, 0 - self.gap - self.offset))
-            self.screen.blit(font.render(str(self.bird[birdID].counter),
-                                         -1,
-                                         (255, 255, 255)),
-                             (200, 50))
-            if self.bird[birdID].dead:
-                self.bird[birdID].sprite = 2
-            elif self.bird[birdID].jump:
-                self.bird[birdID].sprite = 1
-            self.screen.blit(self.bird[birdID].birdSprites[self.bird[birdID].sprite], (70, self.bird[birdID].birdY))
-            if not self.bird[birdID].dead:
-                self.bird[birdID].sprite = 0
-            self.updateWalls()
-            #maybe go throug array with id
-            
-            self.birdUpdate(birdID)
+                (self.wallx, 0 - self.gap - self.offset))
+            #maybe global counter and safe if dies
+            self.screen.blit(font.render(str(self.bird[0].counter),
+                           -1,
+                            (255, 255, 255)),
+                            (200, 50))
+            for i in range(0,numberOfPlayers) :
+                if self.bird[i].dead:
+                    self.bird[i].sprite = 2
+                elif self.bird[i].jump:
+                    self.bird[i].sprite = 1
+                self.screen.blit(self.bird[i].birdSprites[self.bird[i].sprite], (self.bird[i].positionX, self.bird[i].birdY))
+                if not self.bird[i].dead:
+                    self.bird[i].sprite = 0
+                self.updateWalls()
+                self.birdUpdate(i)               
             pygame.display.update()
 
 if __name__ == "__main__":
